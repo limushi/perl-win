b63f9ce3976618ed70ea796325b2ecfb82dd69db
limushi <maxlianov@gmail.com>
2024-03-01 11:03:47 +0300

    Create test.pl
    let's start :)

---
+++ test.pl
@@ -0,0 +1,9 @@
+#!perl
+
+print("Hi, Max! It's a SuperBot!\n\n");
+
+# echo local tasks or from Gmail, or something else
+print("Today tasks:\n");
+print("Today emails:\n");
+
+print("\nWhat are you plan to do? ;)\n");
