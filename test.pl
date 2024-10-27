4e5760e13502ba46387d58f954de34dd149e4c74
Maxim Lianov <maxlianov@gmail.com>
2024-10-27 17:55:43 +0300

    example of tasks file in Latin

--- test.pl
+++ test.pl
@@ -1,13 +1,35 @@
 #!perl
+#use warnings;
+#use strict;
 
 #todo check local config with functions
 #todo прочитать конфиг, если он есть локально, и подгрузить необходимые функции
+#набор первых функций - напоминалки из файла tasks - задачи
 
 print("Hi, Max! It's a SuperBot!\n\n");
 
 # echo local tasks or from Gmail, or something else
+#codepages :)
+$filename = 'tasks.txt';
+
+if (-e $filename) {
+    open (FILE, $filename);
+        while (<FILE>) {
+            chomp;
+            ($id, $task, $data) = split(';');
+            print "ID: $id\n";
+            print "Task number: $task\n";
+            print "Note: $data\n";
+            print "---------\n";
+            }
+        close (FILE);
+    
 print("Today tasks:\n");
+}
+
 print("Today emails:\n");
 
 print("\nWhat are you plan to do? ;)\n");
 print("\ntest\n");
+
+exit;
